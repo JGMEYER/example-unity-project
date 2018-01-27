9583b82ec4f4b24f8baa8c0b71f597549c38ed59
therouxc <theroux.chris@gmail.com>
2018-01-27 16:08:48 -0500

    Setting up IceGame UI and victory condition

--- Example Unity Project/Assets/Scripts/IceGameManager.cs
+++ Example Unity Project/Assets/Scripts/IceGameManager.cs
@@ -1,22 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class IceGameManager : MonoBehaviour {
 
     public int numPlayers;
+    public int playersDead = 0;
+    public Text victoryText;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         GameObject[] players = GameObject.FindGameObjectsWithTag("IcePlayer");
-        for (int i = 3; i >= 0 + numPlayers; i--)
+        for (int i = 3; i >= numPlayers; i--)
         {
             Destroy(players[i]);
         }
+        victoryText.text = "";
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+	
 	}
+
+    public void handlePlayerDeath(string playerName) {
+        playersDead++;
+        if (playersDead >= numPlayers - 1) {
+            victoryText.text = "Game Over!";
+        }
+    }
 }
--- Example Unity Project/Assets/Scripts/IcePlayer.cs
+++ Example Unity Project/Assets/Scripts/IcePlayer.cs
@@ -1,15 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
-public class IcePlayer : MonoBehaviour
-{
+public class IcePlayer : MonoBehaviour {
 
     public float speed;
     public string inputKeys = "wasd";
     public Vector3 spawnPoint;
     public float collisionForce;
     public int numLife = 3;
+    public Text lifeText;
 
     private Rigidbody rb;
     private string left;
@@ -18,22 +19,19 @@ public class IcePlayer : MonoBehaviour
     private string down;
     private Vector3 currentPosition;
 
-    void Start()
-    {
+    void Start() {
         rb = GetComponent<Rigidbody>();
         transform.position = spawnPoint;
+        updateLifeText();
     }
 
-    void FixedUpdate()
-    {
+    void FixedUpdate() {
         float moveHorizontal = 0;
         float moveVertical = 0;
-        if (inputKeys.Equals("arrows"))
-        {
+        if (inputKeys.Equals("arrows")) {
             moveHorizontal = Input.GetAxis("Horizontal");
             moveVertical = Input.GetAxis("Vertical");
-        }
-        else if (inputKeys.Equals("wasd")) {
+        } else if (inputKeys.Equals("wasd")) {
             moveHorizontal = Input.GetAxis("Horizontal2");
             moveVertical = Input.GetAxis("Vertical2");
         }
@@ -42,10 +40,8 @@ public class IcePlayer : MonoBehaviour
         rb.AddForce(movement * speed);
     }
 
-    void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.name.StartsWith("Player"))
-        {
+    void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.name.StartsWith("Player")) {
             Vector3 dir = collision.contacts[0].point - transform.position;
             dir = -dir.normalized;
             dir.y = 0;
@@ -53,42 +49,34 @@ public class IcePlayer : MonoBehaviour
         }
     }
 
-    //void OnTriggerEnter(Collider other)
-    //{
-      //  if (other.gameObject.name.Equals("Player1") || other.gameObject.name.Equals("Player2"))
-        //{
-          //  Vector3 otherPos = other.transform.position;
-           // Vector3 dif = transform.position - otherPos;
-            //rb.AddForce((dif) * 50);
-       // }
-   // }
-
-    void Update()
-    {
+    void Update() {
         currentPosition = transform.position;
-        if (currentPosition.y < -20)
-        {
+        if (currentPosition.y < -20) {
             handleDeath();
         }
     }
 
-    public Rigidbody GetRigidBody()
-    {
+    public Rigidbody GetRigidBody() {
         return rb;
     }
 
-    private void handleDeath()
-    {
-        if (numLife >= 1)
-        {
+    private void handleDeath() {
+        numLife--;
+        updateLifeText();
+
+        if (numLife > 0) {
             transform.position = spawnPoint;
             rb.velocity = Vector3.zero;
             rb.angularVelocity = Vector3.zero;
-            numLife--;
-        }
-        else
-        {
+        } else {
+            GameObject manager = GameObject.Find("IceGameManager");
+            IceGameManager iceManager = (IceGameManager)manager.GetComponent(typeof(IceGameManager));
+            iceManager.handlePlayerDeath(this.name);
             Destroy(this);
         }
+    }
+
+    private void updateLifeText() {
+        lifeText.text = this.name + " Lives: " + numLife.ToString();
     }
 }
