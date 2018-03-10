3f250433cce8af3f102d66053641efe87f6d6ea7
Justian Meyer <meyer.justian@gmail.com>
2018-03-10 13:32:41 -0800

    [LightMazeGame] Game speed controller for debugging

---
+++ Example Unity Project/Assets/Scripts/GameSpeedController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSpeedController : MonoBehaviour {
+
+	public bool controlSpeed = true;
+	[Range(0.25f, 4f)]
+	public float timeScale = 1f;
+
+	float _lastTimeScale;
+
+	private void Start() {
+		_lastTimeScale = timeScale;
+	}
+	
+	void Update () {
+		if (controlSpeed = true && _lastTimeScale != timeScale) {
+			Time.timeScale = timeScale;
+		}
+	}
+}
