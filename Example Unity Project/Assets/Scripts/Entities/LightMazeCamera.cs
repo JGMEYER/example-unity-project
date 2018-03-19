dbc17af9d43db2bc26ea83cbef3cba544609fc8c
JGMEYER <meyer.justian@gmail.com>
2018-03-19 14:06:24 -0700

    [LightMazeGame] Move camera logic into its own script

---
+++ Example Unity Project/Assets/Scripts/Entities/LightMazeCamera.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+
+public class LightMazeCamera : MonoBehaviour {
+
+    [SerializeField]
+    private LightMazeMap _map;
+    private Camera _camera;
+
+    void Start() {
+        _camera = GetComponent<Camera>();
+	}
+
+    void Update() {
+        Vector3 cameraPos = _camera.transform.position;
+        _camera.transform.position = new Vector3((float)_map.mapWidth / 2 - 0.5f, cameraPos.y, cameraPos.z);
+	}
+}
--- Example Unity Project/Assets/Scripts/SceneManagers/LightMazeGameManager.cs
+++ Example Unity Project/Assets/Scripts/SceneManagers/LightMazeGameManager.cs
@@ -38,9 +38,6 @@ public class LightMazeGameManager : MonoBehaviour {
 	}
 
 	void Update() {
-		Vector3 cameraPos = _camera.transform.position;
-		_camera.transform.position = new Vector3((float)_map.mapWidth / 2 - 0.5f, cameraPos.y, cameraPos.z);
-
 		DoInput();
 
 		if (!_gameOver) {
