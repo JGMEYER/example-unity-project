e1259bcef5b752dc4e160b2e05e58dd8f256d853
Justian Meyer <meyer.justian@gmail.com>
2018-03-26 02:22:00 -0700

    [General] Display possible login key configs from player select UI
    Hacky, but works. Just doesn't display the names some of the weirder values like "Return" or "Semicolon" very well.

--- Example Unity Project/Assets/Scripts/UI/PlayerSelect.cs
+++ Example Unity Project/Assets/Scripts/UI/PlayerSelect.cs
@@ -8,13 +8,18 @@ using UnityEngine.UI;
 public class PlayerSelect : MonoBehaviour
 {
 
+    [SerializeField]
+    private GameObject uiPlayerKeyboardControlsLayoutPrefab;
+    [SerializeField]
+    private GameObject uiPlayerJoystickControlsLayoutPrefab;
+
     private GlobalControls globalControls;
 
     private void Start()
     {
         globalControls = InputManager.Instance.GlobalControls();
 
-        UpdateText();
+        UpdatePlayerSelectUI();
     }
 
     private void Update()
@@ -28,37 +33,113 @@ public class PlayerSelect : MonoBehaviour
 
     private void OnEnable()
     {
-        InputEventManager.Instance.StartListening(InputEvent.PlayerControlsAssigned, UpdateText);
+        InputEventManager.Instance.StartListening(InputEvent.PlayerControlsAssigned, UpdatePlayerSelectUI);
     }
 
     private void OnDisable()
     {
-        InputEventManager.Instance.StopListening(InputEvent.PlayerControlsAssigned, UpdateText);
+        InputEventManager.Instance.StopListening(InputEvent.PlayerControlsAssigned, UpdatePlayerSelectUI);
     }
 
-    // Hacky proof-of-concept demo.
-    private void UpdateText()
+    private void UpdatePlayerSelectUI()
     {
-        foreach (PlayerNumber playerNumber in Enum.GetValues(typeof(PlayerNumber)))
+        Dictionary<PlayerNumber, IPlayerControls> playerControlsAssignments = InputManager.Instance.PlayerControlsAssignments;
+        List<IPlayerControls> availablePlayerControls = InputManager.Instance.AvailablePlayerControls;
+
+        foreach (PlayerNumber playerNumber in Enum.GetValues(typeof(PlayerNumber))) {
+            UpdatePlayerPanel(playerNumber, playerControlsAssignments[playerNumber], availablePlayerControls);
+        }
+    }
+
+    // Hacky proof-of-concept demo
+    private void UpdatePlayerPanel(PlayerNumber playerNumber, IPlayerControls playerControls, List<IPlayerControls> availablePlayerControls)
+    {
+        // Hacky: coupling with GameObject name
+        GameObject playerPanel = GameObject.Find("Player" + (int)playerNumber + " Panel");
+        Vector3 playerPanelPosition = playerPanel.transform.position;
+
+        // Clear panel
+        foreach (Transform child in playerPanel.transform)
         {
-            Text playerText = GameObject.Find("Player" + (int)playerNumber + " Text").GetComponent<Text>();
-            playerText.text = "Player" + (int)playerNumber + ": ";
+            Destroy(child.gameObject);
+        }
 
-            IPlayerControls playerControlsAssignment = InputManager.Instance.PlayerControlsAssignments[playerNumber];
+        if (playerControls == null)
+        {
+            // Hacky: assumes both prefabs same height
+            float controlsLayoutHeight = uiPlayerKeyboardControlsLayoutPrefab.GetComponent<RectTransform>().rect.height;
+            float controlsLayoutBuffer = 20f;
 
-            if (playerControlsAssignment == null)
+            // Starting Y position
+            float controlsLayoutY = ((controlsLayoutHeight * availablePlayerControls.Count) + (controlsLayoutBuffer * (availablePlayerControls.Count - 1))) / 2;
+
+            foreach (IPlayerControls availableControls in availablePlayerControls)
             {
-                playerText.text += "Join with: ";
-                foreach (IPlayerControls playerControls in InputManager.Instance.AvailablePlayerControls)
+                if (availableControls is PlayerKeyboardControls)
                 {
-                    playerText.text += playerControls.GetJoinGameKeyName() + " ";
+                    PlayerKeyboardControls playerKeyboardControls = (PlayerKeyboardControls)availableControls;
+
+                    // Hacky: grabs Panel by its Image
+                    GameObject keyboardControlsLayout = Instantiate(uiPlayerKeyboardControlsLayoutPrefab);
+                    Image[] uiKeys = keyboardControlsLayout.GetComponentsInChildren<Image>();
+
+                    foreach (Image uiKey in uiKeys)
+                    {
+                        Text uiKeyText = uiKey.GetComponentInChildren<Text>();
+
+                        // Hacky: key names should be enums or standardized in some way
+                        switch(uiKey.name)
+                        {
+                            case "SubmitKey":
+                                uiKeyText.text = playerKeyboardControls.SubmitKey.ToString();
+                                break;
+                            case "UpKey":
+                                uiKeyText.text = playerKeyboardControls.UpKey.ToString();
+                                break;
+                            case "LeftKey":
+                                uiKeyText.text = playerKeyboardControls.LeftKey.ToString();
+                                break;
+                            case "DownKey":
+                                uiKeyText.text = playerKeyboardControls.DownKey.ToString();
+                                break;
+                            case "RightKey":
+                                uiKeyText.text = playerKeyboardControls.RightKey.ToString();
+                                break;
+                        }
+                    }
+
+                    keyboardControlsLayout.transform.SetParent(playerPanel.transform);
+                    keyboardControlsLayout.transform.localPosition = new Vector3(0, controlsLayoutY, 0);
                 }
-            }
-            else
-            {
-                playerText.text += "[Assigned " + playerControlsAssignment.GetType().ToString() + "]";
+                else if (availableControls is PlayerJoystickControls)
+                {
+                    GameObject joystickControlsLayout = Instantiate(uiPlayerJoystickControlsLayoutPrefab);
+
+                    joystickControlsLayout.transform.SetParent(playerPanel.transform);
+                    joystickControlsLayout.transform.localPosition = new Vector3(0, controlsLayoutY, 0);
+                }
+                else
+                {
+                    throw new ArgumentException("Unknown player controls type.");
+                }
+
+                controlsLayoutY -= controlsLayoutHeight + controlsLayoutBuffer;
             }
         }
-    } 
+        else
+        {
+            GameObject textGameObject = new GameObject("Joined Text");
+            Text joinedText = textGameObject.AddComponent<Text>();
+
+            // Hacky: should be put in a prefab or something
+            joinedText.text = "Player" + (int)playerNumber + " Joined\n" + playerControls.GetType().ToString();
+            joinedText.font = Resources.GetBuiltinResource(typeof(Font), "Arial.ttf") as Font;
+            joinedText.alignment = TextAnchor.MiddleCenter;
+            joinedText.color = Color.black;
+
+            joinedText.transform.SetParent(playerPanel.transform);
+            joinedText.transform.localPosition = Vector3.zero;
+        }
+    }
 
 }
